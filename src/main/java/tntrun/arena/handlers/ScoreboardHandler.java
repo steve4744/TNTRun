c05ab18503e97e248ed8c0d7acaffa83984a33f2
BuildTools <steve4744@gmail.com>
2022-06-22 17:43:44 +0100

    update StringUtils to commons.lang3

--- src/main/java/tntrun/arena/handlers/ScoreboardHandler.java
+++ src/main/java/tntrun/arena/handlers/ScoreboardHandler.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
--- src/main/java/tntrun/utils/Utils.java
+++ src/main/java/tntrun/utils/Utils.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
