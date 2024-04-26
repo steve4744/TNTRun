94b1de08ca21f59d49930cb810f094fcb278936b
BuildTools <steve4744@gmail.com>
2024-04-26 17:17:37 +0100

    update spigot version checker for java 21

--- src/main/java/tntrun/VersionChecker.java
+++ src/main/java/tntrun/VersionChecker.java
@@ -3,7 +3,7 @@ package tntrun;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
 import org.bukkit.Bukkit;
 
 public class VersionChecker {
@@ -20,7 +20,7 @@ public class VersionChecker {
 
 	public String getVersion() {
 		try {
-			HttpURLConnection con = (HttpURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=53359").openConnection();
+			HttpURLConnection con = (HttpURLConnection) new URI("https://api.spigotmc.org/legacy/update.php?resource=53359").toURL().openConnection();
 			con.setDoOutput(true);
 			con.setRequestMethod("GET");
 			String version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
